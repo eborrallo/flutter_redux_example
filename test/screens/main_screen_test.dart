f497d43665b66926bbd0dd84cc47b9b4fdf4c8c1
Enric Borrallo Rodriguez <borrallorodriguez@gmail.com>
2020-11-21 21:50:23 +0100

    no message

--- test/screens/main_screen_test.dart
+++ test/screens/main_screen_test.dart
@@ -10,39 +10,60 @@ import 'package:redux/redux.dart';
 
 void main() {
   group('Main screen', () {
-    testWidgets('We can see the widgest in the main ',
-        (WidgetTester tester) async {
-      // Build our app and trigger a frame.
+    Widget sut;
+    setUp(() {
       Timer(const Duration(seconds: 5), () {});
 
       const Key innerKey = Key('main');
-      MainScreen sut = MainScreen(key: innerKey);
+      MainScreen mainScreen = MainScreen(key: innerKey);
       Store<AppState> store = new Store(
         appReducer,
         initialState: AppState(),
       );
 
-      await tester.pumpWidget(StoreProvider<AppState>(
+      sut = StoreProvider<AppState>(
           store: store,
           child: MaterialApp(
             home: Scaffold(
               appBar: AppBar(title: const Text('Title')),
-              body: sut,
+              body: mainScreen,
             ),
-          )));
+          ));
+    });
+    group('Home tab', () {
+      testWidgets('We can see the "On progress" widgest in the home tab ',
+          (WidgetTester tester) async {
+        await tester.pumpWidget(sut);
+        await tester.pumpAndSettle();
 
-      expect(find.text('On progress'), findsOneWidget);
-      expect(find.text('Almost Due'), findsOneWidget);
-      expect(find.text('Today Calss'), findsOneWidget);
+        expect(find.text('On progress'), findsOneWidget);
+      });
+      testWidgets('We can see the "Almost Due" widgest in the home tab ',
+          (WidgetTester tester) async {
+        await tester.pumpWidget(sut);
+        await tester.pumpAndSettle();
+
+        expect(find.text('Almost Due'), findsOneWidget);
+      });
+      testWidgets('We can see the "Today Class" widgest in the home tab ',
+          (WidgetTester tester) async {
+        await tester.pumpWidget(sut);
+
+        expect(find.text('Today Calss'), findsOneWidget);
+      });
+    });
+    testWidgets('We can see the "bottom bar" widgest in the home tab ',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(sut);
+
       var addButton = find.widgetWithIcon(FloatingActionButton, Icons.add);
-      print(addButton);
       expect(addButton, findsOneWidget);
 
       expect(find.byIcon(Icons.assignment), findsNothing);
       expect(find.byIcon(Icons.assignment_ind), findsNothing);
       expect(find.byIcon(Icons.assignment_turned_in), findsNothing);
       await tester.tap(addButton);
-      await tester.pumpAndSettle(const Duration(seconds: 5));
+      await tester.pumpAndSettle();
 
       expect(find.byIcon(Icons.assignment), findsOneWidget);
       expect(find.byIcon(Icons.assignment_ind), findsOneWidget);
